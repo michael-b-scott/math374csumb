662b36e5bc8344ca28181061cb11dc38deea0c8a
Michael B. Scott <mscott@csumb.edu>
2023-02-19 20:16:35 -0800

    Initial Commit

---
+++ plot_template.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 05:41:13 2023
+
+@author: mbs
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 19:54:51 2023
+
+@author: mbs
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2, 100)  # Sample data.
+
+# Note that even in the OO-style, we use `.pyplot.figure` to create the Figure.
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.plot(x, x, label='linear')  # Plot some data on the axes.
+ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
+ax.plot(x, x**3, label='cubic')  # ... and some more.
+ax.set_xlabel('x label')  # Add an x-label to the axes.
+ax.set_ylabel('y label')  # Add a y-label to the axes.
+ax.set_title("Simple Plot")  # Add a title to the axes.
+ax.legend();  # Add a legend.
